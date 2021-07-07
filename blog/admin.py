450c60202622f6b0707c6e4aa76beb9161bfa303
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 13:26:55 +0000

    Register Comment and CommentAdmin classes

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Post, Tag
+from . models import Post, Tag, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -15,5 +15,10 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('date',)
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post')
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
