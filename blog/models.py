f1ae43070652371377b6adb257f6b09a21a39dd7
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 13:24:16 +0000

    Create Comment class model

--- blog/models.py
+++ blog/models.py
@@ -24,3 +24,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments',
+                             on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
