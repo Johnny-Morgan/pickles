2b0a59774a4e26c187a734f08bce91b2da389c71
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 15:51:11 +0000

    Add path to delete a blog post comment

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.blog, name='blog'),
     path('<slug:slug>/', views.post, name='post'),
+    path('delete_comment/<int:comment_id>/',
+         views.delete_comment,
+         name='delete_comment'),
 ]
