34c231c0f34fd89bac6eee7a820634821ecfefce
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 12:07:55 +0000

    Add list_display field to PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,13 @@ from . models import Post, Tag
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_display = (
+        'title',
+        'intro',
+        'date',
+        'image',
+    )
+
 
 
 admin.site.register(Post, PostAdmin)
