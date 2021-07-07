fe7f2b9732e7c564ca9c193df2dccdb3d36c240c
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 11:56:27 +0000

    Create Tag model

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=254)
     slug = models.SlugField(unique=True, db_index=True)
     intro = models.TextField(max_length=254)
     body = models.TextField(validators=[MinLengthValidator(15)])
     date = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
     image_url = models.URLField(max_length=1024, blank=True, null=True)
     image = models.ImageField(null=True, blank=True)
 
