fa8e0f6276e9edde5315cc9c01c817d0507de90a
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 00:51:00 +0000

    Add Post model to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=254)
+    slug = models.SlugField(unique=True, db_index=True)
+    intro = models.TextField(max_length=254)
+    body = models.TextField(validators=[MinLengthValidator(15)])
+    date = models.DateTimeField(auto_now_add=True)
+    image_url = models.URLField(max_length=1024, blank=True, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
