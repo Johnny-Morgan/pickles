d5a99d10d20a67dacc40932f1713162fcbfc56c1
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 13:06:41 +0000

    Create blog post view

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,9 @@ def blog(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'blog/blog.html', context)
+
+
+def post(request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {'post': post}
+    return render(request, 'blog/post.html', context)
