ff4d18d93522fec8b0ee1369d3a8c869cd70c387
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 15:50:20 +0000

    Add view to delete a blog post comment

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Post
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Post, Comment
 from .forms import CommentForm
 
 
@@ -24,10 +24,21 @@ def post(request, slug):
             return redirect('post', slug=post.slug)
     else:
         form = CommentForm()
-    
+
     context = {
         'post': post,
         'comments': comments,
         'form': form
     }
     return render(request, 'blog/post.html', context)
+
+
+def delete_comment(request, comment_id):
+    """ Delete a comment from a blog post """
+    if not request.user.is_superuser:
+        return redirect(reverse('blog'))
+
+    comment = get_object_or_404(Comment, pk=comment_id)
+    comment.delete()
+
+    return redirect(reverse('blog'))
