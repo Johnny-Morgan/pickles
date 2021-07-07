9966ef5bd4be85fd56657842f06045929696a3c1
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-07 16:11:57 +0000

    Create PostForm class in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Post
 
 
 class CommentForm(forms.ModelForm):
@@ -11,3 +11,9 @@ class CommentForm(forms.ModelForm):
             'email': 'Your Email',
             'body': 'Your Comment',
         }
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['slug']
