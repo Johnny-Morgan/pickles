96e13efe29e3de2e184183c3512a34fda2bbbb75
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-08 14:46:49 +0000

    Add Tag and Post string method tests

--- blog/test_models.py
+++ blog/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Post
+from .models import Post, Tag
 
 
 class TestPostModel(TestCase):
@@ -11,3 +11,13 @@ class TestPostModel(TestCase):
     def test_post_creation(self):
         post = self.create_post()
         self.assertTrue(isinstance(post, Post))
+
+    def test_tag_string_method_returns_title(self):
+        tag = Tag.objects.create(title='Test Blog Tag')
+        self.assertEqual(str(tag), 'Test Blog Tag')
+
+    def test_post_string_method_returns_title(self):
+        post = Post.objects.create(title='Title',
+                                   intro='intro',
+                                   body='abcdefghijklmno')
+        self.assertEqual(str(post), 'Title')
