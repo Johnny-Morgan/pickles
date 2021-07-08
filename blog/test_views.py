047666fe2105b2f65117205e4843bac05d41ac78
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-08 13:59:07 +0000

    Add tests for blog app views

---
+++ blog/test_views.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from .models import Post
+
+
+class TestViews(TestCase):
+
+    def test_get_blog_page(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog.html')
+
+    def test_get_post_page(self):
+        post = Post.objects.create(title='title', slug='title',
+                                   intro='intro', body='abcdefghijklmno')
+        response = self.client.get(f'/blog/{post.slug}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post.html')
+
+    def test_get_edit_post_page(self):
+        post = Post.objects.create(title='title', slug='title',
+                                   intro='intro', body='abcdefghijklmno')
+        response = self.client.get(f'/blog/edit/{post.slug}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/edit_post.html')
+
+    def test_can_add_post(self):
+        response = self.client.post('/blog/',
+                                    {'title': 'title', 'intro': 'intro',
+                                     'body': 'abcdefghijklmno'})
+        self.assertRedirects(response, '/blog/')
+
+    def test_can_delete_post(self):
+        post = Post.objects.create(title='title', slug='title',
+                                   intro='intro', body='abcdefghijklmno')
+        response = self.client.get(f'/blog/delete_post/{post.id}/')
+        self.assertRedirects(response, '/blog/')
+        existing_items = Post.objects.filter(id=post.id)
+        self.assertEqual(len(existing_items), 0)
