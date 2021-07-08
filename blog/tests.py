d58d81d17c4e197c252915c27cf35b30645bed42
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-08 11:04:06 +0000

    Test PostForm class

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from .forms import PostForm
 
-# Create your tests here.
+
+class TestPostForm(TestCase):
+
+    def test_item_title_is_required(self):
+        form = PostForm({
+            'title': '',
+            'intro': 'intro',
+            'body': 'abcdefghijklmno',
+            })
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_item_intro_is_required(self):
+        form = PostForm({
+            'title': 'title',
+            'intro': '',
+            'body': 'abcdefghijklmno',
+            })
+        self.assertFalse(form.is_valid())
+        self.assertIn('intro', form.errors.keys())
+        self.assertEqual(form.errors['intro'][0], 'This field is required.')
+
+    def test_item_body_is_required(self):
+        form = PostForm({
+            'title': 'title',
+            'intro': 'intro',
+            'body': '',
+            })
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_item_body_is_at_least_15_characters(self):
+        form = PostForm({
+            'title': 'title',
+            'intro': 'intro',
+            'body': 'body',
+            })
+        self.assertFalse(form.is_valid())
+
+    def test_tags_field_is_not_required(self):
+        form = PostForm({
+            'title': 'title',
+            'intro': 'intro',
+            'body': 'abcdefghijklmno',
+            })
+        self.assertTrue(form.is_valid())
