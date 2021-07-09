c19f161f9a97e50dd41cdd866d1b2ead167dac72
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-09 23:01:11 +0000

    Test Tag model in blog app

--- blog/test_models.py
+++ blog/test_models.py
@@ -2,6 +2,20 @@ from django.test import TestCase
 from .models import Post, Tag, Comment
 
 
+class TestTagModel(TestCase):
+
+    def create_tag(self, title='title',):
+        return Tag.objects.create(title=title)
+
+    def test_tag_creation(self):
+        tag = self.create_tag()
+        self.assertTrue(isinstance(tag, Tag))
+
+    def test_tag_string_method_returns_title(self):
+        tag = Tag.objects.create(title='Test Blog Tag')
+        self.assertEqual(str(tag), 'Test Blog Tag')
+
+
 class TestPostModel(TestCase):
     def create_post(self, title='title',
                     intro='intro',
@@ -11,10 +25,6 @@ class TestPostModel(TestCase):
     def test_post_creation(self):
         post = self.create_post()
         self.assertTrue(isinstance(post, Post))
-
-    def test_tag_string_method_returns_title(self):
-        tag = Tag.objects.create(title='Test Blog Tag')
-        self.assertEqual(str(tag), 'Test Blog Tag')
 
     def test_post_string_method_returns_title(self):
         post = Post.objects.create(title='Title',
