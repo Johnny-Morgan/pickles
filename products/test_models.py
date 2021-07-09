f9b114ae05c84494e69d6ee528e63b78e96d443c
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-09 22:48:19 +0000

    Test Product model in products app

--- products/test_models.py
+++ products/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, Product
 
 
 class TestCategoryModel(TestCase):
@@ -15,3 +15,30 @@ class TestCategoryModel(TestCase):
                                            friendly_name='friendly_name')
         self.assertEqual(str(category), 'name')
 
+
+class TestProductModel(TestCase):
+
+    category = Category.objects.create(name='name',
+                                       friendly_name='friendly_name')
+
+    def create_product(self, category=category, sku='sku',
+                       name='name', description='desc',
+                       price=10, on_sale=True,
+                       image_url='url', image='image'):
+        return Product.objects.create(category=category, sku=sku, name=name,
+                                      description=description, price=price,
+                                      on_sale=on_sale, image_url=image_url,
+                                      image=image)
+
+    def test_product_creation(self):
+        product = self.create_product()
+        self.assertTrue(isinstance(product, Product))
+
+    def test_product_string_method_returns_name(self):
+        category = Category.objects.create(name='name',
+                                           friendly_name='friendly_name')
+        product = Product.objects.create(category=category, sku='sku',
+                                         name='name', description='desc',
+                                         price=10, on_sale=True,
+                                         image_url='url', image='image')
+        self.assertEqual(str(product), 'name')
