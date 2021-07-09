8ef2aebfca7e8bf0fd0c896bb44412fa95b64278
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-09 15:21:20 +0000

    Add logic to sort products by price and category

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -12,8 +13,25 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            print(sortkey)
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -28,11 +46,14 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'discount_percentage': 100 - settings.DISCOUNT_PERCENTAGE,
         'search_word': query,
         'categories': categories,
+        'sorting': sorting,
     }
 
     return render(request, 'products/products.html', context)
