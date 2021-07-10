752eaf59a1583d3ec6953f72b2949cd38d8b11d3
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-10 16:53:04 +0000

    Add url to add a product to shopping basket

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_basket, name='view_basket')
+    path('', views.view_basket, name='view_basket'),
+    path('add/<item_id>/', views.add_product_to_basket,
+         name='add_product_to_basket'),
 ]
