7b29faa3976f6b7d85b4bc30f34fa11299409d6f
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-10 16:28:26 +0000

    Add basket context logic

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,29 @@
+from django.conf import settings
+
+
 def basket_contents(request):
 
-    context = {}
+    basket_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total + settings.DELIVERY_COST
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
 
     return context
