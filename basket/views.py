9909a62a958d1fbf622150f5fe8aec99bc5696e6
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-10 12:09:23 +0000

    Create basket view

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_basket(request):
+    """ View that renders the shopping basket contents page """
+    return render(request, 'basket/basket.html')
