7b330fb047684410b7f81ae39619c98d504ffc22
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-14 11:13:10 +0000

    Add method to generate order number to Order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from products.models import Product
 
@@ -21,6 +23,12 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
 
 
 class OrderLineItem(models.Model):
