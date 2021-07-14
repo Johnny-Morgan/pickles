175523ab2db7e522c14b85ea036717e162fc6a25
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-14 11:14:14 +0000

    Add save method to Order and OrderLineItem models

--- checkout/models.py
+++ checkout/models.py
@@ -30,6 +30,15 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False,
@@ -41,3 +50,11 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
                                          editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
