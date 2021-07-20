8a9622a128fe3c8a85226f1ef4284a5aa4b9d42b
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-20 12:53:23 +0000

    Remove secret key and set debug

--- pickles/settings.py
+++ pickles/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d#cp1prx-7bc$gys-^rrw(-&!5ktis6(4w_kvu8hp3vivv^uu2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pickles-eshop.herokuapp.com', 'localhost']
 
