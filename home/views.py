14f4b0eba976fcb4d99870f7a2149bd4a3b2398d
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-20 11:56:58 +0000

    Add custom 404 error template, view and handler

--- home/views.py
+++ home/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    """View to render the home page"""
     return render(request, 'home/index.html')
+
+
+def error_404(request, exception):
+    """View to render a cutom 404 page"""
+    return render(request, '../templates/404.html')
