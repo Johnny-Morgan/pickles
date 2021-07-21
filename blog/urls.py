b7ff36f6523b5ed9ef5ae71073ee9867c12c4189
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-21 14:25:53 +0000

    Add the add_post view path to urlpatterns list

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('add/', views.add_post, name='add_post'),
     path('<slug:slug>/', views.post, name='post'),
     path('delete_comment/<int:comment_id>/',
          views.delete_comment,
