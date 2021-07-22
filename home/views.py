6a8c1f97a85bdd53db5b2ed5b170a80740be9a9c
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-22 15:07:02 +0000

    Add home page mobile device layout

--- home/views.py
+++ home/views.py
@@ -4,9 +4,11 @@ from products.models import Product
 
 def index(request):
     """View to render the home page"""
-    sale_items_set_1 = Product.objects.filter(on_sale=True)[:3]
-    sale_items_set_2 = Product.objects.filter(on_sale=True)[3:6]
+    sale_items = Product.objects.filter(on_sale=True)
+    sale_items_set_1 = sale_items[:3]
+    sale_items_set_2 = sale_items[3:6]
     context = {
+        'sale_items': sale_items,
         'sale_items_set_1': sale_items_set_1,
         'sale_items_set_2': sale_items_set_2,
     }
