fd58381e6bcbc7e4a8bf96c3e1cccb1c45f6fb84
Johnny-Morgan <jwjmorgan@gmail.com>
2021-07-22 14:38:38 +0000

    Remove background image and add carousel to home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """View to render the home page"""
-    return render(request, 'home/index.html')
+    sale_items_set_1 = Product.objects.filter(on_sale=True)[:3]
+    sale_items_set_2 = Product.objects.filter(on_sale=True)[3:6]
+    context = {
+        'sale_items_set_1': sale_items_set_1,
+        'sale_items_set_2': sale_items_set_2,
+    }
+    return render(request, 'home/index.html', context)
 
 
 def error_404(request, exception):
